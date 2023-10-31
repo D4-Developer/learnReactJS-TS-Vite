2df48aa1230441e891163ced41a33c697e5b4280
D4-Developer <darshan.6858@gmail.com>
2023-11-01 01:08:44 +0530

    Fix-Feat: cards are now displayed horizontally. parent-child hierarchy updated in [App.css]

--- app/src/module2/App.tsx
+++ app/src/module2/App.tsx
@@ -5,22 +5,11 @@ import Hero from './components/Hero';
 import Card from './components/Card';
 import experiences from './data/experiences-data';
 
-/*
-	Challenge:
-
-	- import the array of data from <dataFileName>.js
-	- map over the array to create <Card /> components
-	- display the array of card components under the navbar
-		(in place of the current <Card /> component)
-
-	Note: We haven't styled the group of components yet, so they'll
-	still be block elements, stacked vertically. We'll add styling later.
-*/
-
 export default function App(): React.ReactNode {
 	const cards = experiences.map((item) => {
 		return (
 			<Card
+				key={item.id}
 				img={item.coverImg}
 				rating={item.stats.rating}
 				reviewCount={item.stats.reviewCount}
@@ -35,7 +24,9 @@ export default function App(): React.ReactNode {
 		<>
 			<Navbar />
 			<Hero />
-			{cards}
+			<section className="cards-list">
+				{cards}
+			</section>
 		</>
 	);
 }
