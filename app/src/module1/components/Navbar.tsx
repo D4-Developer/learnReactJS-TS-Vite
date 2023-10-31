3c4e2dcbd70fd12ef811bf301858fb09293669a9
darshan.rathod <darshan.rathod@zeuslearning.com>
2023-10-31 17:49:56 +0530

    Initial commit

---
+++ app/src/module1/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <div className="nav-left">
+                <img className="nav-logo" src="../images/react-icon-small.png" />
+                <h3 className="nav-logo-text" >ReactFacts</h3> 
+            </div>
+            <div className="nav-right">
+                <h4>React Course - Project 1</h4>
+            </div>
+        </nav>
+    )
+}
