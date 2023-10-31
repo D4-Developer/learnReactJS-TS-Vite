744515413b8d07d5bf4bbc83d36e8b38679c3b50
D4-Developer <darshan.6858@gmail.com>
2023-10-31 20:40:25 +0530

    Challenge-M2-V4: Build the Card component with the hard-code in the data

--- app/src/module2/App.tsx
+++ app/src/module2/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 
 export default function App(): React.ReactNode {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Card />
 		</>
 	);
 }
---
+++ app/src/module2/components/Card.tsx
@@ -0,0 +1,48 @@
+import cardImg from '../assets/katie-zaferes.png'
+import starImg from '../assets/star.png'
+
+/*
+Challenge: Build the Card component
+For now, hard-code in the data (like 
+the rating, title, price, etc.)
+
+Notes:
+- Only render 1 instance (I already did this for you)
+- The star icon and photo (katie-zaferes.png) are in the images 
+  folder for your use
+- Make sure to include:
+    - image
+    - star icon (star.png), rating, and review count
+    - title
+    - cost/person
+- The main purpose of this challenge is to show you where our limitations
+  currently are, so don't worry about the fact that you're hard-coding all
+  this data into the component.
+*/
+
+
+export default function Card(): React.ReactNode {
+  return (
+    <section className="card">
+
+      <div className="card-img-container">
+        <div>
+          <img className="img" src={cardImg} />
+          <div className="card-status"><span>SOLD OUT</span></div>
+        </div>
+      </div>
+
+      <div className="card-text-container" >
+        <img className="star-img" src={starImg} />
+        <span> 5.0 </span>
+        <span>(6) · USA</span>
+        <div className="card-text-title">Life lessons with Katie Zaferes</div>
+        <div >
+          <span className="card-text-sub1" >From $136 </span>
+          <span className="card-text-sub2" >/ person</span>
+        </div>
+      </div>
+
+    </section>
+  );
+}
