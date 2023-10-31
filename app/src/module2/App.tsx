7727dcf0ae86448cc7ec869f5fd88997d3565a76
D4-Developer <darshan.6858@gmail.com>
2023-11-01 00:38:17 +0530

    Challenge: import the array of data from <dataFileName>.js then map over the array to create <Card /> components

--- app/src/module2/App.tsx
+++ app/src/module2/App.tsx
@@ -1,14 +1,41 @@
 import './App.css'
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import experiences from './data/experiences-data';
+
+/*
+	Challenge:
+
+	- import the array of data from <dataFileName>.js
+	- map over the array to create <Card /> components
+	- display the array of card components under the navbar
+		(in place of the current <Card /> component)
+
+	Note: We haven't styled the group of components yet, so they'll
+	still be block elements, stacked vertically. We'll add styling later.
+*/
 
 export default function App(): React.ReactNode {
+	const cards = experiences.map((item) => {
+		return (
+			<Card
+				img={item.coverImg}
+				rating={item.stats.rating}
+				reviewCount={item.stats.reviewCount}
+				location={item.location}
+				title={item.title}
+				price={item.price}
+			/>
+		);
+	});
+
 	return (
 		<>
 			<Navbar />
 			<Hero />
-			<Card />
+			{cards}
 		</>
 	);
 }
--- app/src/module2/components/Card.tsx
+++ app/src/module2/components/Card.tsx
@@ -1,41 +1,28 @@
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-
-export default function Card(): React.ReactNode {
+export default function Card(props: {
+  img: string,
+  rating: number,
+  reviewCount: number
+  location: string
+  title: string,
+  price: number,
+}): React.ReactNode {
   return (
     <section className="card">
 
       <div className="card-img-container">
         <div>
-          <img className="img" src="./src/module2/assets/katie-zaferes.png" />
+          <img className="img" src={`./src/module2/assets/${props.img}`} />
           <div className="card-status"><span>SOLD OUT</span></div>
         </div>
       </div>
 
       <div className="card-text-container" >
         <img className="star-img" src="./src/module2/assets/star.png" />
-        <span> 5.0 </span>
-        <span>(6) · USA</span>
-        <div className="card-text-title">Life lessons with Katie Zaferes</div>
+        <span> {props.rating} </span>
+        <span>({props.reviewCount}) · {props.location}</span>
+        <div className="card-text-title">{props.title}</div>
         <div >
-          <span className="card-text-sub1" >From $136 </span>
+          <span className="card-text-sub1" >From ${props.price} </span>
           <span className="card-text-sub2" >/ person</span>
         </div>
       </div>
