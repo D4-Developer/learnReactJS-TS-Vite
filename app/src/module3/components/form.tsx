ec478dc8465c264d1fb7ba95d5e48d593a582aaa
D4-Developer <darshan.6858@gmail.com>
2023-11-02 12:06:58 +0530

    Challenge-M3-V6: Get a random image from the `memesData` array on btn-click event

--- app/src/module3/components/form.tsx
+++ app/src/module3/components/form.tsx
@@ -1,11 +1,33 @@
+import memesData from "../../module2/data/memesData";
+
 export default function Form(): React.ReactNode {
+
+  /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+
+  function getMemeImage(): void {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    console.log(memesArray[randomNumber].url);
+  }
+
   return (
     <main className="main-form">
-      <form action="" className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="topText" />
         <input type="text" placeholder="Bottom text" className="bottomText" />
-        <button className="form-btn">Get a new meme image 🏞️</button>
-      </form>
+        <button
+          className="form-btn"
+          onClick={getMemeImage} >
+          Get a new meme image 🏞️
+        </button>
+      </div>
     </main>
   );
 }
