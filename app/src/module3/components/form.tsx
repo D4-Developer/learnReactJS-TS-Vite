91efc9569e44650f7fcb01f8217564eb271c2b83
D4-Developer <darshan.6858@gmail.com>
2023-11-03 20:30:12 +0530

    Challenge: As soon as the Meme component loads the first time, make an API call to "https://api.imgflip.com/get_memes".
    When the data comes in, save just the memes array part of that data to the `allMemes` state
    Fix: by mistake memesData.tsx was stored inside module2. so it was removed and added to the module3/data/

--- app/src/module3/components/form.tsx
+++ app/src/module3/components/form.tsx
@@ -1,25 +1,42 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
-import memesData from "../../module2/data/memesData";
+// import memesData from "../../module3/data/memesData";
 
 export default function Form(): React.ReactNode {
 
   /**
    * Challenge: 
-   * 1. Set up the text inputs to save to
-   *    the `topText` and `bottomText` state variables.
-   * 2. Replace the hard-coded text on the image with
-   *    the text being saved to state.
+   * As soon as the Meme component loads the first time,
+   * make an API call to "https://api.imgflip.com/get_memes".
+   * 
+   * When the data comes in, save just the memes array part
+   * of that data to the `allMemes` state
+   * 
+   * Think about if there are any dependencies that, if they
+   * changed, you'd want to cause to re-run this function.
+   * 
+   * Hint: for now, don't try to use an async/await function.
+   * Instead, use `.then()` blocks to resolve the promises
+   * from using `fetch`. We'll learn why after this challenge.
    */
 
-  const memesArray: {
-    id: string;
-    name: string;
-    url: string;
-    width: number;
-    height: number;
-    box_count: number;
-  }[] = memesData.data.memes;
-  let randomNumber: number = Math.floor(Math.random() * memesArray.length);
+
+  const [allMemes, setAllMeme]: [
+    {
+      id: string;
+      name: string;
+      url: string;
+      width: number;
+      height: number;
+      box_count: number;
+    }[] | [], Dispatch<SetStateAction<{
+      id: string;
+      name: string;
+      url: string;
+      width: number;
+      height: number;
+      box_count: number;
+    }[]>> | Dispatch<SetStateAction<[]>>
+  ] = useState([]);
 
   const [meme, setMeme]: [
     { topText: string, bottomText: string, randomImg: string },
@@ -27,8 +44,16 @@ export default function Form(): React.ReactNode {
   ] = useState({
     topText: "",
     bottomText: "",
-    randomImg: memesArray[randomNumber].url
+    randomImg: "http://i.imgflip.com/1bij.jpg"
   });
+
+  React.useEffect(function () {
+    console.log("Effect ran")
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json())
+      .then(data => { console.log(data); setAllMeme(data.data.memes); })
+  }, []);
+
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value }: { name: string, value: string } = event.target;
@@ -39,10 +64,11 @@ export default function Form(): React.ReactNode {
   }
 
   function getMemeImage(): void {
-    randomNumber = Math.floor(Math.random() * memesArray.length);
+    const randomNumber: number = Math.floor(Math.random() * allMemes.length);
+
     setMeme((prevObj) => ({
       ...prevObj,
-      randomImg: memesArray[randomNumber].url
+      randomImg: allMemes[randomNumber].url
     }));
   }
 
