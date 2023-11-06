71e470d3b07f3bfef6e80c8520103dc09a1ef562
D4-Developer <darshan.6858@gmail.com>
2023-11-06 16:46:47 +0530

    Initial commit- - module4-react-facts project.

--- app/src/module4-react-facts/components/Navbar.tsx
+++ app/src/module4-react-facts/components/Navbar.tsx
@@ -1,13 +1,22 @@
 export default function Navbar(props: { darkMode: boolean }) {
-    return (
-        <nav className={props.darkMode ? "nav dark" : "nav"}>
-            <div className="nav-left">
-                <img className="nav-logo" src="./src/module1/assets/react.svg" />
-                <h3 className="nav-logo-text" >ReactFacts</h3>
-            </div>
-            <div className="nav-right">
-                <h4>React Course - Project 1</h4>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className={props.darkMode ? "nav dark" : "nav"}>
+			<div className="nav-left">
+				<img className="nav-logo" src="./src/module1/assets/react.svg" />
+				<h3 className="nav-logo-text" >ReactFacts</h3>
+			</div>
+			<div className="nav-right">
+				<div className="toggler">
+					<p className="toggler--light">Light</p>
+					<div
+						className="toggler--slider"
+						onClick={props.toggleDarkMode}
+					>
+						<div className="toggler--slider--circle"></div>
+					</div>
+					<p className="toggler--dark">Dark</p>
+				</div>
+			</div>
+		</nav>
+	)
 }
