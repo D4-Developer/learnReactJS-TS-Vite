4e5b3fa9c1fb13632aff6c31839206766b3c1859
D4-Developer <darshan.6858@gmail.com>
2023-11-07 21:54:10 +0530

    Challenge-M4-V31: Create state to hold our array of numbers.

--- app/src/module4-tenzies-game/App.tsx
+++ app/src/module4-tenzies-game/App.tsx
@@ -1,35 +1,40 @@
+import React, { Dispatch, SetStateAction } from "react";
+
 import "./App.css"
 import Die from "./components/Die";
 
+/**
+ * Challenge:
+ * 
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `allNewDice` function so it 
+ * loads all new dice as soon as the app loads)
+ * 
+ * Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
+
 export default function App(): React.ReactNode {
 
-	const dieValues: number[] = [];
-
-	function allNewDice() {
+	function allNewDice(): number[] {
+		const dieValues: number[] = [];
 		for (let index = 0; index < 10; index++) {
 			dieValues.push(Math.ceil(Math.random() * 6));
 		}
+
+		return dieValues;
 	}
 
-	allNewDice();
+	const [dice, setDice]: [number[], Dispatch<SetStateAction<number[]>>] = React.useState(() => allNewDice());
 
-	console.log(dieValues);
-
+	console.log(dice);
 
 	return (
 		<main>
 			<div className="container">
 				<div className="grid">
-					<Die value={dieValues[0]} />
-					<Die value={dieValues[1]} />
-					<Die value={dieValues[2]} />
-					<Die value={dieValues[3]} />
-					<Die value={dieValues[4]} />
-					<Die value={dieValues[5]} />
-					<Die value={dieValues[6]} />
-					<Die value={dieValues[7]} />
-					<Die value={dieValues[8]} />
-					<Die value={dieValues[9]} />
+					{dice.map((die, idx) => <Die key={idx} value={die} />)}
 				</div>
 			</div>
 		</main>
